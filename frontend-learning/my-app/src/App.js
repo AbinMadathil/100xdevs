20b540f33ad634a7dbd5bf12b9ca557a1eb71088
AbinMadathil <abinmadathil7@gmail.com>
2025-06-29 16:53:15 +0000

    Pushing the changes for studying

---
+++ frontend-learning/my-app/src/App.js
@@ -0,0 +1,46 @@
+import logo from './logo.svg';
+import './App.css';
+import React, { useState } from 'react';
+
+function App() {
+  //this is a basic todo app using React, that was built for studying purposes.
+
+  const [toDos, setToDos] = useState([
+    {
+      title: "Learn React",
+      description: "Learn React by building a simple app", 
+      completed: false
+    },
+    {
+      title:"Learn Node",
+      description: "Learn Node by building a simple backend",
+      completed: false
+    }
+  ]);
+
+  function AddTODo(){
+  setToDos([...toDos,{
+    title:"Randodm ToDo",
+    description: "This is a random ToDo item",
+    completed: false
+  }]);
+}
+
+  return (
+    <div className="App">
+          <button onClick={AddTODo}> add a random todo</button>
+      {toDos.map(function(todo){ 
+        return <HandleAddToDo title={todo.title} description={todo.description} />
+        })}
+    </div>
+  );
+}
+
+function HandleAddToDo(props){
+  return <div>
+      <h1>{props.title}</h1>
+      <div>{props.description}</div>
+    </div>
+}
+
+export default App;
